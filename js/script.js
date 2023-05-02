2f700a568d0d04c299d9afb1e0a648ff34f00a89
tropicthunderbix <132273278+tropicthunderbix@users.noreply.github.com>
2023-05-02 06:12:09 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 
-const states = document.querySelectorAll(".switch-state .item");
+prev.classList.add("disable");
 
-prev.addEventListener("click", function () {
-  console.log("prev");
-});
-next.addEventListener("click", function () {
-  console.log("prev");
-});
+const states = document.querySelectorAll(".switch-state .item");
+const imgStates = document.querySelectorAll(".switch-state .img");
 
 const swiper = new Swiper(".formslider", {
   autoHeight: true,
+  allowTouchMove: false,
   navigation: {
     nextEl: ".next",
     prevEl: ".prev",
@@ -19,6 +16,19 @@ const swiper = new Swiper(".formslider", {
 });
 
 swiper.on("slideChange", function () {
-  states.forEach((state) => state.classList.remove("active"))
-  states[swiper.realIndex].classList.add('active')
+  imgStates.forEach((state) => state.classList.remove("active"));
+  states.forEach((state) => state.classList.remove("active"));
+  
+  imgStates[swiper.realIndex].classList.add("active");
+  states[swiper.realIndex].classList.add("active");
+
+  next.classList.remove("disable");
+  prev.classList.remove("disable");
+
+  if (swiper.realIndex === 0) {
+    prev.classList.add("disable");
+  }
+  if (swiper.realIndex === states.length - 1) {
+    next.classList.add("disable");
+  }
 });
